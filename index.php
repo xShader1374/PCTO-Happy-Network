36e974dc86ebd34f63a7e370540058d037f374ad
xShader1374 <shader1374@gmail.com>
2024-11-12 09:27:57 +0100

    Update css

--- index.php
+++ index.php
@@ -1,18 +1,27 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Sito SerieTV&Film</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link href="eds-www/style.css" rel="stylesheet">
+    <link href="style.css" rel="stylesheet">
     <title>Document</title>
 </head>
 <body>
-    <h1>Sito Film!</h1>
-    <button>csc</button>
-    <?php
-        echo "ciao";
-    ?>
+    <div class="Navbar-Container" id="Navbar-Container">
+        <Button class="Navbar-Button">Esplora</Button>
+        |
+        <Button class="Navbar-Button">Serie TV</Button>
+        |
+        <Button class="Navbar-Button">Film</Button>
+    </div>
+    <div class="Body-Container" id="Body-Container">
+        <?php
+            echo "ciao";
+        ?>
+    </div>
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha384-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/IYI/Cxo=" crossorigin="anonymus"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
