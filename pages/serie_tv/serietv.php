70dd38da758ff28305a03752f20c56a839ec69e9
xShader1374 <shader1374@gmail.com>
2024-11-13 16:47:19 +0100

    Update serietv.php

--- pages/serie_tv/serietv.php
+++ pages/serie_tv/serietv.php
@@ -1,14 +1,96 @@
 <?php
 
-$categorie_solo_serietv = array("azione", "drammatico", "fantasy", "horror");
+$categorie_solo_serie_tv_json = '{
+    "azione": [
+        {
+            "title": "Titolo1",
+            "description": "Descrizione1",
+            "img_url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRgTE5iCv4FF2t_zcBubldZ7asQz6xC1jpTuQ&s"
+        },
+        {
+            "title": "Titolo2",
+            "description": "Descrizione2",
+            "img_url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRgTE5iCv4FF2t_zcBubldZ7asQz6xC1jpTuQ&s"
+        }
+    ],
+    "drammatico": [
+        {
+            "title": "Titolo1",
+            "description": "Descrizione1",
+            "img_url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRgTE5iCv4FF2t_zcBubldZ7asQz6xC1jpTuQ&s"
+        },
+        {
+            "title": "Titolo2",
+            "description": "Descrizione2",
+            "img_url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRgTE5iCv4FF2t_zcBubldZ7asQz6xC1jpTuQ&s"
+        },
+        {
+            "title": "Titolo3",
+            "description": "Descrizione3",
+            "img_url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRgTE5iCv4FF2t_zcBubldZ7asQz6xC1jpTuQ&s"
+        },
+        {
+            "title": "Titolo4",
+            "description": "Descrizione4",
+            "img_url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRgTE5iCv4FF2t_zcBubldZ7asQz6xC1jpTuQ&s"
+        }
+    ],
+    "horror": [
+        {
+            "title": "Titolo1",
+            "description": "Descrizione1",
+            "img_url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRgTE5iCv4FF2t_zcBubldZ7asQz6xC1jpTuQ&s"
+        },
+        {
+            "title": "Titolo2",
+            "description": "Descrizione2",
+            "img_url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRgTE5iCv4FF2t_zcBubldZ7asQz6xC1jpTuQ&s"
+        },
+        {
+            "title": "Titolo3",
+            "description": "Descrizione3",
+            "img_url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRgTE5iCv4FF2t_zcBubldZ7asQz6xC1jpTuQ&s"
+        },
+        {
+            "title": "Titolo4",
+            "description": "Descrizione4",
+            "img_url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRgTE5iCv4FF2t_zcBubldZ7asQz6xC1jpTuQ&s"
+        }
+    ],
+    "romantico": [
+        {
+            "title": "Titolo1",
+            "description": "Descrizione1",
+            "img_url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRgTE5iCv4FF2t_zcBubldZ7asQz6xC1jpTuQ&s"
+        },
+        {
+            "title": "Titolo2",
+            "description": "Descrizione2",
+            "img_url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRgTE5iCv4FF2t_zcBubldZ7asQz6xC1jpTuQ&s"
+        },
+        {
+            "title": "Titolo3",
+            "description": "Descrizione3",
+            "img_url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRgTE5iCv4FF2t_zcBubldZ7asQz6xC1jpTuQ&s"
+        }
+    ]
+}';
 
-foreach ($categorie_solo_serietv as $index => $categoria) {
-    if ($index == 0) {
+$categorie_solo_serie_tv = json_decode($categorie_solo_serie_tv_json, true);
+
+$i = 0;
+
+foreach ($categorie_solo_serie_tv as $index => $categoria) {
+	$nome_categoria = $index;
+
+	if ($i == 0) {
+		$i++;
         include("templates/section.php");
         continue;
     }
     
     echo '<div class="Separator unselectable"></div>';
 	include("templates/section.php");
+	$i++;
 }
 ?>
