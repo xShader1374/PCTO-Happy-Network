3a2bdda4abd67c3d2271f3a00fbc0266d86c7b27
xShader1374 <shader1374@gmail.com>
2024-11-13 16:52:03 +0100

    update

--- templates/card.php
+++ templates/card.php
@@ -7,6 +7,6 @@
 <div class="media-card">
 	<h5 class="media-title unselectable"><?= $card["title"] ?></h5>
 	<img class="media-image unselectable" src="<?= $card["img_url"] ?>" alt="media-image">
-	<div class="Separator unselectable"></div>
+	<div class="Separator-Card unselectable"></div>
 	<p class="media-description unselectable"><?= $card["description"] ?></p>
 </div>
