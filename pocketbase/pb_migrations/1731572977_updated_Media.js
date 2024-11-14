c46b4901a17151b47981cd6100aa28ad59be4f26
Francesco Cruciata <francescogiovannicruciata@gmail.com>
2024-11-14 10:05:12 +0100

    correzioni

---
+++ pocketbase/pb_migrations/1731572977_updated_Media.js
@@ -0,0 +1,52 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("ozwdlg8re5zcssq")
+
+  // remove
+  collection.schema.removeField("99tvzs5i")
+
+  // add
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "o54cy8ax",
+    "name": "tipo",
+    "type": "select",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "maxSelect": 1,
+      "values": [
+        "film",
+        "serie_tv"
+      ]
+    }
+  }))
+
+  return dao.saveCollection(collection)
+}, (db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("ozwdlg8re5zcssq")
+
+  // add
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "99tvzs5i",
+    "name": "tipo",
+    "type": "text",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "min": null,
+      "max": null,
+      "pattern": ""
+    }
+  }))
+
+  // remove
+  collection.schema.removeField("o54cy8ax")
+
+  return dao.saveCollection(collection)
+})
