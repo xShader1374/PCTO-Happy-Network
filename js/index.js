29de0511cd693f796fee01c677b34559abf81015
xShader1374 <shader1374@gmail.com>
2024-11-15 13:02:14 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -4,6 +4,7 @@ var modal_title = document.getElementById('modal-title');
 var modal_trailer = document.getElementById('modal-iframe');
 var modal_description = document.getElementById('modal-description');
 
+// Vuoto nel caso sia un film
 var modal_lista_ep_stag = document.getElementById('listaEpisodi-Stagioni');
 
 async function getSeasons(media_id) {
@@ -32,37 +33,60 @@ async function updateModalInfo(title, trailer_url, description, media_id, type) 
     // TODO: Una chiamata per prendere le stagioni e per ogni stagione fare una chiamata per prendere la lista degli episodi e per ogni episodio fare una chiamata per prendere il nome dell'episodio
 
     if (type === "serie_tv") {
-        const stagioni = await getSeasons(media_id);
+        var stagioni = await getSeasons(media_id);
+        stagioni = stagioni.items;
+
+        console.log(stagioni);
         
-        console.log(stagioni.items);
+        var titoli_e_id = [];
 
-        /*
+        var episodi = [];
 
-        const episodi = getEpisodes(season_id);
+        stagioni.forEach((stagione, index) => {
+            var titolo = `${stagione.nome} (${stagione.n_episodi})`;
+            console.log(`${index + 1}) ${titolo} serie_tv: ${stagione.serie_tv}`);
+            var temp = {
+                "titolo": titolo,
+                "id": stagione.serie_tv
+            }
+            titoli_e_id.push(temp);
+        });
 
+        console.log(titoli_e_id);
+
+        asynctitoli_e_id.forEach(stagione_info => {
+            var temp_ep = await getEpisodes(stagione_info.id);
+            episodi.push(temp_ep);
+        });
+
+        var episodi = getEpisodes(season_id);
+        
         modal_lista_ep_stag.innerHTML = episodi
             .map(episode => `
-            <div class="accordion-item-dark">
-                <h2 class="accordion-header">
-                    <button class="accordion-button ${episode.index !== 1 ? 'collapsed' : ''} btn Navbar-Button" 
-                            type="button" 
-                            data-bs-toggle="collapse" 
-                            data-bs-target="#collapse${episode.index}" 
-                            aria-expanded="${episode.index === 1 ? 'true' : 'false'}" 
-                            aria-controls="collapse${episode.index}">
-                        ${episode.title}
-                    </button>
-                </h2>
-                <div id="collapse${episode.index}" 
-                     class="accordion-collapse collapse ${episode.index === 1 ? 'show' : ''}" 
-                     data-bs-parent="#accordionExample">
-                    <div class="accordion-body">
-                        ${episode.content}
+            <div class="accordion" id="listaEpisodi-Stagioni">
+				<div class="accordion-item-dark">
+                    <h2 class="accordion-header">
+                        <button class="accordion-button ${episode.index !== 1 ? 'collapsed' : ''} btn Navbar-Button" 
+                                type="button" 
+                                data-bs-toggle="collapse" 
+                                data-bs-target="#collapse${episode.index}" 
+                                aria-expanded="${episode.index === 1 ? 'true' : 'false'}" 
+                                aria-controls="collapse${episode.index}">
+                            ${episode.title}
+                        </button>
+                    </h2>
+                    <div id="collapse${episode.index}" 
+                        class="accordion-collapse collapse ${episode.index === 1 ? 'show' : ''}" 
+                        data-bs-parent="#accordionExample">
+                        <div class="accordion-body">
+                            ${episode.content}
+                        </div>
                     </div>
-                </div>
-            </div>
+                </div>			
+			</div>
+            
         `).join('');
-        */
+        
     }
 }
 
